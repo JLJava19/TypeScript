19f73230c89838229dae55d03a67f44e3cd96269
Jose <jlhm2077@gmail.com>
2019-11-04 09:18:43 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var Visualizer = window["ui-router-visualizer"].Visualizer;
+var app = angular.module("repaso", ["ui.router"]);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,5 @@
+let Visualizer = window["ui-router-visualizer"].Visualizer;
+const app = angular.module("repaso",         // Nombre de la app o modulo principal
+                            ["ui.router"]);  // Inyeccion de modulos o librerias
+
+
