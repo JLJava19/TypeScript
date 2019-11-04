314022016ade29116340a43f23cbba93dcc91b2d
Jose <jlhm2077@gmail.com>
2019-11-04 13:55:25 +0100

    filtros contratos

--- controllers/ContratosController.js
+++ controllers/ContratosController.js
@@ -5,6 +5,46 @@ var ContratosController = (function () {
         $scope.vm = this;
         $scope.vm.titulo = "Contratos Titulo";
         $scope.vm.contratos = $scope.vm.contratosJson;
+        $scope.vm.contratosMapeados = $scope.vm.contratos.map(function (elem) {
+            return {
+                "id": elem.idColumn,
+                "nombre": (elem.nombre) ? elem.nombre : "Sin nombre",
+                "numeroAcciones": (elem.ACCIONES) ? elem.ACCIONES.length : 0
+            };
+        });
+        console.debug('contratos mapeados %o' + $scope.vm.contratosMapeados);
+        $scope.vm.contratosSinAcciones = $scope.vm.contratos.filter(function (v) {
+            if (v.ACCIONES == undefined) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        $scope.vm.contratosAccionesEntre1y3 = $scope.vm.contratos.filter(function (v) {
+            if (v.ACCIONES != undefined) {
+                return (v.ACCIONES.length >= 1 && v.ACCIONES.length <= 3);
+            }
+            else {
+                return false;
+            }
+        });
+        $scope.vm.contratosAccionesMayor3 = $scope.vm.contratos.filter(function (v) {
+            if (v.ACCIONES != undefined) {
+                return (v.ACCIONES.length > 3);
+            }
+            else {
+                return false;
+            }
+        });
+        $scope.vm.contratosAccionesClaveSituacion = $scope.vm.contratos.filter(function (v) {
+            if (v.ACCIONES == undefined) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
     }
     ContratosController.$inject = ["$scope", "contratosJson"];
     return ContratosController;
--- controllers/ContratosController.ts
+++ controllers/ContratosController.ts
@@ -1,3 +1,9 @@
+interface IContratoResumen{
+    id: string;
+    nombre: string;
+    numeroAcciones: number;
+}
+
 interface IContratosController extends ng.IScope{
     vm: ContratosController;
 }
@@ -6,6 +12,11 @@ class ContratosController implements ng.IController{
 
     public titulo: string;
     public contratos: any;
+    public contratosMapeados: Array<IContratoResumen>;
+    public contratosSinAcciones: any;
+    public contratosAccionesEntre1y3: any;
+    public contratosAccionesMayor3: any;
+    public contratosAccionesClaveSituacion: any;
 
     public static $inject = ["$scope", "contratosJson"];
 
@@ -13,6 +24,65 @@ class ContratosController implements ng.IController{
         $scope.vm = this;
         $scope.vm.titulo = "Contratos Titulo";
         $scope.vm.contratos = $scope.vm.contratosJson;
+    
+        // Contratos ordenados a nuestro gusto para poder ordenar
+        $scope.vm.contratosMapeados = $scope.vm.contratos.map( (elem)=>{
+            return {
+                    "id":elem.idColumn,
+                    "nombre":(elem.nombre)?elem.nombre:"Sin nombre",
+                    "numeroAcciones":(elem.ACCIONES)?elem.ACCIONES.length:0
+                    }
+
+        } );
+        console.debug('contratos mapeados %o' + $scope.vm.contratosMapeados);
+
+        // Numero de contratos sin acciones
+        $scope.vm.contratosSinAcciones = $scope.vm.contratos.filter( (v)=>{
+            if ( v.ACCIONES == undefined ){
+
+                return  true;
+                   
+              }else{
+                  return false;
+              }
+        } );
+        // Numero de contratos entre 1 y 3 acciones
+        $scope.vm.contratosAccionesEntre1y3 = $scope.vm.contratos.filter( (v)=>{
+            if ( v.ACCIONES != undefined ){
+
+                return  (v.ACCIONES.length >= 1 && v.ACCIONES.length <= 3) ;
+                   
+              }else{
+                  return false;
+              }
+        } );
+        // Numero de contratos de > 3 caracteres
+        
+        $scope.vm.contratosAccionesMayor3 = $scope.vm.contratos.filter( (v)=>{
+            if ( v.ACCIONES != undefined ){
+
+                return  (v.ACCIONES.length > 3) ;
+                   
+              }else{
+                  return false;
+              }
+        } );
+        // Todas las acciones diferentes
+        
+        // Buscar Primer contrato que tenga en ACCIONES "clave":situacion
+        $scope.vm.contratosAccionesClaveSituacion = $scope.vm.contratos.filter( (v)=>{
+            if ( v.ACCIONES == undefined ){
+
+                return  true;
+                   
+              }else{
+                  return false;
+              }
+        } );
+        //.map( e => e.ACCIONES.clave!=undefined );
+        //.find( (e)=>{e.ACCIONES.clave=="SITUACION"} );
+        // Buscar último contrato que tenga en ACCIONES "clave":situacion
+
     } //Constructor
 
 }
